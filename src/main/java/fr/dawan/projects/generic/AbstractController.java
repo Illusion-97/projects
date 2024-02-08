67413c5b40ab65db38676b48c4bac3d75af563a3
Yanis Adekalom <yanis971.60@gmail.com>
2024-02-08 11:21:13 +0100

    Feat : ExceptionHandler

--- src/main/java/fr/dawan/projects/generic/AbstractController.java
+++ src/main/java/fr/dawan/projects/generic/AbstractController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 public abstract class AbstractController<D,S extends GenericService<D>> {
@@ -16,5 +18,16 @@ public abstract class AbstractController<D,S extends GenericService<D>> {
         // Model permet la transmission d'informations entre le programme et le template
         model.addAttribute("elements", service.findAll(Pageable.unpaged()));
         return path + "/all";
+    }
+
+    @GetMapping("/remove/{id}")
+    public String delete(@PathVariable long id) {
+        service.deleteById(id);
+        return "redirect:/" + path;
+    }
+
+    @ModelAttribute("path")
+    public String getPath() {
+        return path;
     }
 }
